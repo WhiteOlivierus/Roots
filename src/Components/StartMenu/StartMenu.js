5eec07f85fb1cb616f44f31b319594daf723e066
WhiteOlivierus <bas.dijkstra@student.hku.nl>
2021-06-23 10:20:59 +0200

    Added chromium detector

--- src/Components/StartMenu/StartMenu.js
+++ src/Components/StartMenu/StartMenu.js
@@ -1,14 +1,52 @@
+import PropTypes from "prop-types";
 import * as React from "react";
 import * as MUI from "@material-ui/core";
+import * as Router from "react-router-dom";
 
 import CreateProject from "./CreateProject";
+import chromiumDetector from "chromium-detector";
 
 import { Logo } from "./Logo";
 import { Recent } from "./Recent";
 import { useOpen } from "../FlowEditor/MenuBar/useOpen";
+import { useModal } from "react-modal-hook";
 
-const StartMenu = () => {
+const StartMenu = ({ history }) => {
     const [open, handleOpen, handleClose] = useOpen(false);
+
+    const [showModal] = useModal(({ in: open, onExited }) => (
+        <MUI.Dialog open={open} onExited={onExited}>
+            <MUI.DialogTitle>No chromium detected</MUI.DialogTitle>
+            <MUI.DialogContent>
+                <MUI.Typography>
+                    Roots only works with chromium at the moment. Please install
+                    a chromium based browser to use Roots.
+                </MUI.Typography>
+            </MUI.DialogContent>
+            <MUI.DialogActions>
+                <MUI.Button
+                    variant="contained"
+                    onClick={() => {
+                        window.location.href = "https://www.google.com/chrome/";
+                    }}
+                >
+                    Get Chrome
+                </MUI.Button>
+                <MUI.Button
+                    variant="outlined"
+                    onClick={() => history.push("/")}
+                >
+                    Back
+                </MUI.Button>
+            </MUI.DialogActions>
+        </MUI.Dialog>
+    ));
+
+    React.useEffect(() => {
+        const info = chromiumDetector.getBrowserInfo();
+        if (!info.isChromium) showModal();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
     return (
         <MUI.Box bgcolor="primary.main" width={1} height="100vh">
@@ -33,6 +71,10 @@ const StartMenu = () => {
     );
 };
 
+StartMenu.propTypes = {
+    history: PropTypes.object,
+};
+
 StartMenu.displayName = "StartMenu";
 
-export default React.memo(StartMenu);
+export default React.memo(Router.withRouter(StartMenu));
